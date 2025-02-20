fcc8e77cc007d8386e1225075bbbc4ee1625de39
unknown <manafikhiamjad@gmail.com>
2025-02-21 00:02:41 +0300

    css instead of useEffect for step-name

--- src/Steps.jsx
+++ src/Steps.jsx
@@ -3,31 +3,33 @@ import "./Steps.css"
 
 export default function Steps (props){
     const style={backgroundColor:"rgb(191,226,253)" , color:"rgb(72,62,255)"};
-    const [stepName,setStepName]= React.useState(true);
+    /*
+    onther way to handle step-name on mobile design
+    const [stepName,setStepName]= React.useState(()=> handleResize(window.innerWidth));
+    function handleResize (width) {
+        console.log(width);
+        if (Math.round(width) >= 650) {
+          return true;
+        } else {
+          return false;
+        }
+      }; 
     React.useEffect(()=>{
-        function handleResize (width) {
-            console.log(width);
-            if (Math.round(width) >= 650) {
-              setStepName(true);
-            } else {
-              setStepName(false);
-            }
-          }; 
         
-          window.addEventListener('resize' , ()=> handleResize(window.innerWidth));
-          return window.removeEventListener('resize' ,() => handleResize(window.innerWidth));
+          window.addEventListener('resize' , ()=> setStepName(handleResize(window.innerWidth)));
+          return window.removeEventListener('resize' ,() => setStepName(handleResize(window.innerWidth)));
         
-    },[])
+    },[])*/
 
 
     return(
         <>
             <div className="step-card" onClick={()=>props.onClick(props.id)}>
                 <div className="step-circle" style={props.checked ?style: null }>{props.id}</div>
-                {stepName && <div className="step-name">
+                <div className="step-name">
                     <p className="step-number">Step {props.id}</p>
                     <h4 className="step-value">{props.value}</h4>
-                </div>}
+                </div>
             </div>
         </>
     )
